d1841faf5778d9e97172e325d31ca8183ab829ec
aerovir <aeroviradmin@gmail.com>
2018-09-06 15:35:14 +0200

    run

---
+++ config/web.php
@@ -0,0 +1,7 @@
+<?php
+
+
+    return [
+        'id'        => 'DB',
+        'basePath'  => realpath(__DIR__ . '/../')
+    ];
--- web/index.php
+++ web/index.php
@@ -1,1 +1,7 @@
-Videoscool
+<?php
+
+    require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+    $config = require __DIR__ . '/../config/web.php';
+
+    (new yii\web\Application($config))->run();
+?>
