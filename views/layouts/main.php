675314c584ca67ee5998c381b6d4479433f6cd89
aerovir <aeroviradmin@gmail.com>
2018-09-07 10:47:06 +0200

    add bootstrap

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,26 @@
+<?php use yii\bootstrap\NavBar; ?>
+<?php $this->beginPage(); ?>
 <html>
     <head>
         <title>Добрая баня</title>
+        <?php $this->head(); ?>
     </head>
     <body>
+    <?php $this->beginBody(); ?>
+    <?php
+        NavBar::begin([
+            'brandLabel'    => 'Добрая Баня',
+            'brandUrl'      => Yii::$app->homeUrl,
+            'options'       => [
+                    'class' => 'navbar-default navbar-fixed-top'
+                ]
+            ]);
+        NavBar::end();
+    ?>
+
         <?= $content ?>
 
     </body>
+    <?php $this->endBody(); ?>
 </html>
+<?php $this->endPage(); ?>
