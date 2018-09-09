4a7fecfe7e3504135af4b2db1caead00c0a5b877
aerovir <aeroviradmin@gmail.com>
2018-09-09 21:06:56 +0200

    add table and migrate

--- config/console.php
+++ config/console.php
@@ -3,17 +3,6 @@ return [
     'id' => 'dobrayabanya-console',
     'basePath' => dirname(__DIR__),
     'components' => [
-    'db'     => [
-        'class' => 'yii\db\Connection',
-        'dsn'      => 'mysql:host=localhost;dbname=baseDb',
-        'username' => 'root',
-        'password' => '',
-//                'enableSchemaCache'   => true,
-//                'schemaCacheDuration' => 3600,
-//                'tablePrefix'         => '',
-//                'charset'             => 'utf8mb4',
-//                'enableQueryCache'    => true,
-//                'queryCacheDuration'  => 600,
-    ]
+            'db' => require (__DIR__ . '/db.php')
     ],
 ];
---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+'class' => 'yii\db\Connection',
+            'dsn'      => 'mysql:host=localhost;dbname=baseDb',
+            'username' => 'root',
+            'password' => '',
+//                'enableSchemaCache'   => true,
+//                'schemaCacheDuration' => 3600,
+//                'tablePrefix'         => '',
+//                'charset'             => 'utf8mb4',
+//                'enableQueryCache'    => true,
+//                'queryCacheDuration'  => 600,
+];
