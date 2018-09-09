6b4bbd48c6f2d9928310838c339f4e5302a9783b
aerovir <aeroviradmin@gmail.com>
2018-09-09 10:21:05 +0200

    add user join step object

--- tests/_support/Step/Acceptance/TestUserJoin.php
+++ tests/_support/Step/Acceptance/TestUserJoin.php
@@ -7,33 +7,53 @@ class TestUserJoin extends \AcceptanceTester
     public function imagineUser()
     {
         $I = $this;
-        $user = [];
+        $user = [
+            'name' => 'John',
+            'email' => 'mail@mail.rf',
+            'password' => '123456'
+        ];
+        print_r($user);
         return $user;
     }
 
     public function joinUser($user)
     {
         $I = $this;
+        $I->amOnPage('/user/join');
+        $I->see('Join us');
+        $I->fillField('UserJoinForm[name]', $user['name']);
+        $I->fillField('UserJoinForm[name]', $user['email']);
+        $I->fillField('UserJoinForm[name]', $user['password']);
+        $I->fillField('UserJoinForm[name]', $user['password2']);
+        $I->click('Create');
     }
 
     public function loginUser($user)
     {
         $I = $this;
+        $I->amOnPage('/user/login');
+        $I->see('Log in');
+        $I->fillField('UserLoginForm[name]', $user['email']);
+        $I->fillField('UserLoginForm[name]', $user['password']);
+        $I->click('Enter');
     }
 
     public function logoutUser()
     {
         $I = $this;
+        $I->click('Logout');
     }
 
     public function isUserLogged($user)
     {
         $I = $this;
+        $I->see($user['name']);
     }
 
     public function noUserLogged($user)
     {
         $I = $this;
+        $I->dontSee($user['name']);
     }
 
 }
